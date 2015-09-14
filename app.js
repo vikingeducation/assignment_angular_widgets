b13f50ad39e1c32f1c7ec6dcc3dcfc81011588e8
David <davidmeza@outlook.com>
2015-09-14 12:29:14 -0400

    restaurant creation and deletion working

---
+++ app.js
@@ -0,0 +1,34 @@
+var widgets = angular.module('widgets', []);
+
+
+widgets.controller('RestaurantCtrl',
+    ['$scope',
+    function($scope){
+
+      $scope.restaurants = [];
+      $scope.orderQuery = $scope.restaurantName;
+
+      $scope.createRestaurant = function () {
+        var newRestaurant = {}
+        newRestaurant.name = $scope.restaurantName
+        newRestaurant.foodType = $scope.restaurantFoodType
+        newRestaurant.image = $scope.restaurantImg
+        $scope.restaurants.push(newRestaurant)
+        $scope.restaurantName = ""
+        $scope.restaurantFoodType = ""
+        $scope.restaurantImg = ""
+      }
+
+      $scope.deleteRestaurant = function (index) {
+        $scope.restaurants.splice(index, 1);
+      }
+
+      $scope.sortByName = function () {
+        $scope.orderQuery = "name"
+      }
+
+      $scope.sortByType = function () {
+        $scope.orderQuery = "foodType"
+      }
+
+    }]);
