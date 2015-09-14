c23c351150f54f9ffafd565fab19d8ff1026506c
Alice Fung <alice.o.fung@gmail.com>
2015-09-14 12:56:01 -0400

    restaurants widget complete

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ widgets.controller("RestaurantCtrl", ["$scope", function($scope){
   $scope.restaurants = [];
   $scope.name ="";
   $scope.foodType = "";
-  $scope.restaurantImage = ""
-  $scope.foodTypeFilter = "";
-    $scope.createRestaurant = function(){
+  $scope.restaurantImage = "";
 
+  $scope.foodTypeFilter = "";
+
+  $scope.sort = function(colName){
+    $scope.restaurants.sort(function(a, b) {
+      var textA = a[colName].toUpperCase();
+      var textB = b[colName].toUpperCase();
+      if (colName == 'name'){
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+      }
+      else if (colName == 'foodType'){
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+      }
+    });
+  };
+
+  $scope.createRestaurant = function(){
 
     $scope.restaurants.push({
       name: $scope.name,
@@ -19,9 +33,10 @@ widgets.controller("RestaurantCtrl", ["$scope", function($scope){
 
   };
 
+
   $scope.removeRestaurant = function(index){
-    $scope.restaurants.splice(index, 1)
-  }
+    $scope.restaurants.splice(index, 1);
+  };
 
 
 }]);
