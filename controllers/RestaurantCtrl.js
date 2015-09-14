a5b63044c37d0e2615f56caa385b39c4670c216f
ayva <olga.ayva@gmail.com>
2015-09-14 10:22:44 -0700

    first widget finished

--- controllers/RestaurantCtrl.js
+++ controllers/RestaurantCtrl.js
@@ -1,21 +1,19 @@
 
 widgets.controller('RestaurantCtrl',['$scope', function($scope){
 $scope.restaurants = [];
-$scope.foodName = "";
-$scope.foodType = "";
-$scope.icon = "";
-$scope.fun = function(){
-  var newObj = {
-    name: $scope.foodName,
-    type: $scope.foodType,
-    icon: $scope.icon
-  };
-  $scope.restaurants.push(newObj);
-  $scope.foodName = "";
-  $scope.foodType = "";
+$scope.nameFilter = "";
+$scope.typeFilter = "";
+$scope.addRestaurant = function(){
+
+  $scope.restaurants.push($scope.newRestaurant);
+  $scope.newRestaurant = {};
 };
 $scope.removeRestaurant = function(index){
   $scope.restaurants.splice(index, 1);
-}
+};
+$scope.Sorting = function(source){
+  $scope.SortWay = source;
+};
+$scope.SortWay = "type";
 }
 ]);
