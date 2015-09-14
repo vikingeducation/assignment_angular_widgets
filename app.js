20dd9d28f6298a8aa4a84de7dc6b2571353ca571
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-09-14 12:27:38 -0400

    pictures working

--- app.js
+++ app.js
@@ -11,9 +11,16 @@ widgets.controller('RestaurantCtrl',
       var newRestaurant = {};
       newRestaurant.name = $scope.name;
       newRestaurant.foodType = $scope.foodType;
+      newRestaurant.img = $scope.img;
       $scope.restaurants.push(newRestaurant);
       $scope.name = null;
       $scope.foodType = null;
+      $scope.img = null;
     };
+
+    $scope.deleteRestaurant = function(restaurant){
+      var index = $scope.restaurants.indexOf(restaurant);
+      $scope.restaurants.splice(index, 1);
+    }
 
   }]);
