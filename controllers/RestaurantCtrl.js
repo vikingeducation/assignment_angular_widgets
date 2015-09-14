b4aaeae1ff95786c568c571c6e6d1716bc79d68e
facingsouth <xin.d.liu@gmail.com>
2015-09-14 09:29:43 -0700

    add delete

--- controllers/RestaurantCtrl.js
+++ controllers/RestaurantCtrl.js
@@ -3,14 +3,19 @@ widgets.controller('RestaurantCtrl',['$scope', function($scope){
 $scope.restaurants = [];
 $scope.foodName = "";
 $scope.foodType = "";
+$scope.icon = "";
 $scope.fun = function(){
   var newObj = {
     name: $scope.foodName,
-    type: $scope.foodType
+    type: $scope.foodType,
+    icon: $scope.icon
   };
   $scope.restaurants.push(newObj);
   $scope.foodName = "";
   $scope.foodType = "";
 };
+$scope.removeRestaurant = function(index){
+  $scope.restaurants.splice(index, 1);
+}
 }
 ]);
