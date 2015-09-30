1c5ba1ba78393a9db4d914e8b9f3cbc2ee7092a8
Adam <adam.kinson@gmail.com>
2015-09-29 21:51:28 -0400

    finish restaurant widget

---
+++ app.js
@@ -0,0 +1,33 @@
+var widgets = angular.module('widgets', []);
+
+widgets.controller('RestaurantCtrl',
+  ['$scope',
+  function($scope){
+
+    $scope.restaurants = [];
+    $scope.sort = "foodType";
+
+    $scope.createRestaurant = function(name, foodType, imageURL) {
+      var newRestaurant = {
+        name: name,
+        foodType: foodType,
+        imageURL: imageURL
+      };
+
+      $scope.restaurants.push(newRestaurant);
+      $scope.name = null;
+      $scope.foodType = null;
+      $scope.imageURL = null;
+    };
+
+    $scope.remove = function(restaurant) {
+      var i = $scope.restaurants.indexOf(restaurant);
+      console.log(i)
+      $scope.restaurants.splice(i, 1);
+    };
+
+    $scope.setSort = function(selected) {
+      $scope.sort = selected;
+    };
+
+  }]);
