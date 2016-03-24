1fe14c1bfcbc38c7c38b058e3d0c33a254bdb880
Kit Langton <kitlangton@gmail.com>
2016-03-24 13:42:06 -0400

    finish photos page

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
 var app = angular.module('RestaurantApp', []);
 
-app.controller('RestaurantCtrl', 
-  ["$scope", 
+app.controller('RestaurantCtrl',
+    ["$scope",
     function($scope) {
-    $scope.restaurants = [];
-    $scope.foodName = "";
-    $scope.foodType = "";
-    $scope.createFood = function() {
-       var foodObj = {
-        name: $scope.foodName,
-        type: $scope.foodType,
-       };
-       $scope.restaurants.push(foodObj);
-    }
-    $scope.deleteRestaurant = function(restaurant) {
-      var index = $scope.restaurants.indexOf(restaurant);
-      $scope.restaurants.splice(index,1);
-    }
-  }]);
+      $scope.restaurants = [
+      {name: "McDonalds",
+        type: "Burgers",},
+      {name: "Raymond's",
+        type: "Diner",},
+      {name: "Wendy's",
+        type: "Tacos",},
+      {name: "Chipotle",
+        type: "Ebola",},
+      ];
+
+      $scope.createFood = function() {
+        var restaurantObj = {
+          name: $scope.name,
+          type: $scope.foodType,
+          url: $scope.imageUrl,
+        };
+        $scope.name = "";
+        $scope.foodType = "";
+        $scope.imageUrl = "";
+        $scope.restaurants.push(restaurantObj);
+      }
+
+      $scope.changeOrder = function(filter) {
+        $scope.orderSelection = filter;
+      };
+
+      $scope.deleteRestaurant = function(restaurant) {
+        var index = $scope.restaurants.indexOf(restaurant);
+        $scope.restaurants.splice(index,1);
+      }
+    }]);
+
+
+app.controller('PhotosCtrl',
+    ['$scope',
+    function($scope) {
+      $scope.rawFeed = instagramResponse['data'];
+
+    }])
