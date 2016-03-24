7dda7a189748b5b33b83a72194125c937fa1f8a8
koziscool <kozjob@yahoo.com>
2016-03-24 11:54:54 -0500

    again

--- app.js
+++ app.js
@@ -5,9 +5,20 @@ var widgets = angular.module('widgetsApp', []);
 var restaurantCtrl = widgets.controller('RestaurantCtrl',
     ['$scope',
     function($scope){
+        //stuff is gonna go here
       $scope.restaurants = [];
       $scope.name = "jimmys";
       $scope.foodType = "italian";
-        //stuff is gonna go here
+      $scope.addRestaurant = function( ) {
+        $scope.restaurants.push( {
+          name: $scope.name,
+          foodType: $scope.foodType
+        })
+        console.log($scope.restaurants);
+        console.log($scope.name);
+        console.log($scope.foodType);
+        $scope.name = "";
+        $scope.foodType = "";
+      }
     }]);
 
