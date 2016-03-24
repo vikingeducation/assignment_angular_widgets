4c56d4e1842b2ff84678670df16609b275979367
Thomas Lo <thomasjinlo@gmail.com>
2016-03-24 16:01:20 -0700

    Finish filters

--- app.js
+++ app.js
@@ -39,40 +39,42 @@ widgets.controller('PhotosCtrl',
     $scope.photos = [];
     $scope.filters = [];
     $scope.tags = [];
+    
+    $scope.rawFeed.forEach( function(el) {
+      var photo = {  };
 
-    (function() {
-      $scope.rawFeed.forEach( function(el) {
-        var photo = {  };
+      photo.userName = el['user']['username'];
 
-        photo.userName = el['user']['username'];
+      if (el["caption"] === null) {
+        photo.desc = "";
+      } else {
+        photo.desc = el['caption']['text'];
+      }
 
-        if (el["caption"] === null) {
-          photo.desc = "";
-        } else {
-          photo.desc = el['caption']['text'];
+      photo.imageUrl = el['images']['standard_resolution']['url'];
+      photo.createdTime = el['created_time'];
+      photo.link = el['link'];
+      photo.likes = el['likes']['count'];
+      photo.comments = el['comments']['count'];
+      photo.filter = el['filter'];
+
+      if (el['tags'].length == 0) {
+        photo.tags = ['no tags'];
+      } else {
+        photo.tags = el['tags'];
+      }
+
+      for ( var i = 0; i < photo.tags.length; i++ ) {
+        if ( $scope.tags.indexOf( photo.tags[i] ) === -1 ) {
+          $scope.tags.push( photo.tags[i] );
         }
+      }
 
-        photo.imageUrl = el['images']['standard_resolution']['url'];
-        photo.createdTime = el['created_time'];
-        photo.link = el['link'];
-        photo.likes = el['likes']['count'];
-        photo.comments = el['comments']['count'];
-        photo.filter = el['filter'];
-        photo.photoTags = el['tags'];
+      if ( $scope.filters.indexOf( photo.filter ) === -1 ) {
+        $scope.filters.push( photo.filter );
+      }
 
-        for ( var i = 0; i < photo.photoTags.length; i++ ) {
-          if ( $scope.tags.indexOf( photo.photoTags[i] ) === -1 ) {
-            $scope.tags.push( photo.photoTags[i] );
-          }
-        }
-
-        if ( $scope.filters.indexOf( photo.filter ) === -1 ) {
-          $scope.filters.push( photo.filter );
-        }
-
-        $scope.photos.push(photo);
-      } )
-
-    })();
+      $scope.photos.push(photo);
+    } )
 
   }]);
