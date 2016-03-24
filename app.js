954d991ddc9bff7c3bab9ed781ac6ffaeb24f3ec
Kelsey James <kelseybjames@gmail.com>
2016-03-24 10:17:15 -0700

    widget 1 done

--- app.js
+++ app.js
@@ -3,17 +3,24 @@ var widgets = angular.module('widgets', []);
 widgets.controller('RestaurantCtrl',
 ['$scope',
   function($scope) {
+    $scope.headers = ['name', 'foodType', 'Delete', 'Image']
     $scope.restaurants = [];
+    $scope.orderHeading = 'name';
     $scope.createRestaurant = function() {
       var newObj = {};
       newObj.name = $scope.name;
-      newObj.foodType = $scope.typeOfFood;
+      newObj.foodType = $scope.foodType;
       $scope.restaurants.push(newObj);
       $scope.name = "";
-      $scope.typeOfFood = "";
+      $scope.foodType = "";
     };
     $scope.removeRestaurant = function(index) {
       $scope.restaurants.splice(index, 1);
     };
+
+
+    $scope.orderByHeading = function(index) {
+      $scope.orderHeading = $scope.headers[index];
+    }
   }
 ]);
