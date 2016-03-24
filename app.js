388877fc53eeca1dba8989b518cd751ec282ee53
Julia Herron <julia.herron@gmail.com>
2016-03-24 11:15:02 -0700

    click to sort

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ var widgets = angular.module('widgetsApp', []);
 widgets.controller('RestaurantCtrl',
     ['$scope',
     function($scope){
-      $scope.restaurants = [];
+      $scope.restaurants = [
+        {"name": "john", "foodType": "mexican"}, 
+        {"name": "julia", "foodType": "hamburgers"}, 
+        {"name": "aaaa", "foodType": "aaaaa"}, 
+        {"name": "bbbb", "foodType": "BBB"}];
       $scope.name = "jimmys";
       $scope.foodType = "italian";
       $scope.img = "http://findicons.com/files/icons/7/dinosaurs_toys/128/stegosaurus.png";
@@ -22,6 +26,24 @@ widgets.controller('RestaurantCtrl',
 
       $scope.deleteRestaurant = function(index) {
         $scope.restaurants.splice(index, 1)
+      };
+
+      $scope.sortType = "foodType"
+      $scope.sortOrder = "+";
+      $scope.sortBy = $scope.sortOrder + $scope.sortType
+
+      $scope.changeSortOrder = function(column) {
+        console.log(column)
+        // if you click on the same sortType, change sortOrder
+        if (column === $scope.sortType) {
+          $scope.sortOrder == "+" ? $scope.sortOrder = "-" : $scope.sortOrder = "+";
+        }
+        // if you click on a different sortType, change sortType and default to ascending
+        else {
+          $scope.sortType = column;
+          $scope.sortOrder = "+";
+        }
+        $scope.sortBy = $scope.sortOrder + $scope.sortType
       }
     }]);
 
