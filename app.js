3b02c1a54a928a7eaef5ca842519c31ed66cb5bc
Julia Herron <julia.herron@gmail.com>
2016-03-24 10:29:56 -0700

    image src

--- app.js
+++ app.js
@@ -5,20 +5,23 @@ var widgets = angular.module('widgetsApp', []);
 widgets.controller('RestaurantCtrl',
     ['$scope',
     function($scope){
-        //stuff is gonna go here
       $scope.restaurants = [];
       $scope.name = "jimmys";
       $scope.foodType = "italian";
-      $scope.addRestaurant = function( ) {
-        $scope.restaurants.push( {
-          name: $scope.name,
-          foodType: $scope.foodType
-        })
-        console.log($scope.restaurants);
-        console.log($scope.name);
-        console.log($scope.foodType);
+      $scope.img = "http://findicons.com/files/icons/7/dinosaurs_toys/128/stegosaurus.png";
+      $scope.addRestaurant = function(restaurant) {
+        $scope.restaurants.push( 
+          restaurant
+          // name: $scope.name,
+          // foodType: $scope.foodType
+        )
         $scope.name = "";
         $scope.foodType = "";
+        $scope.img = "";
+      };
+
+      $scope.deleteRestaurant = function(index) {
+        $scope.restaurants.splice(index, 1)
       }
     }]);
 
