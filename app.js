aeefafb74806eebc5fbeaa58094e1d326bb3439b
Jeffrey Gisin <jgisin@gmail.com>
2016-03-24 18:00:10 -0600

    done

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ widgets.controller('RestaurantCtrl',
 
 widgets.controller('PhotosCtrl', ['$scope', function($scope){
   $scope.rawFeed = instagramResponse.data;
-
+  $scope.currentPage = 0;
   $scope.hashtags = function(){
     var tagArray = [];
     $scope.rawFeed.forEach(function(obj){
@@ -37,11 +37,65 @@ widgets.controller('PhotosCtrl', ['$scope', function($scope){
           tagArray.push(obj.tags[i]);
         }
       }
-    })
+    });
     return tagArray;
   };
 
-  $scope.photos = $scope.rawFeed.map(function(obj){
+  var setFilter = function(){
+    $scope.filterPhotos = $scope.rawFeed.filter(function(obj, index){
+      if(index > $scope.currentPage * 12 - 1 && index < ($scope.currentPage + 1) * 12){
+        console.log(index);
+        return true;
+      }
+      else {
+        return false;
+      }
+    });
+  };
+
+  var setPhoto = function(){
+    $scope.photos = $scope.filterPhotos.map(function(obj){
+      return {
+        url: obj.images.low_resolution.url,
+        username: obj.user.username,
+        createdAt:  obj.created_time,
+        likes: obj.likes.count,
+        comments: obj.comments.count,
+        photoLink: obj.link,
+        tags: obj.tags.join(', '),
+        filter: obj.filter
+      };
+    });
+  };
+
+  $scope.filterPhotos = $scope.rawFeed.filter(function(obj, index){
+    if(index > $scope.currentPage * 12 - 1 && index < ($scope.currentPage + 1) * 12){
+      return true;
+    }
+    else {
+      return false;
+    }
+  });
+
+  $scope.addPage = function(){
+    console.log('works', $scope.currentPage);
+    if($scope.currentPage <= ($scope.rawFeed.length / 12) -1){
+      $scope.currentPage += 1;
+      setFilter();
+      setPhoto();
+    }
+  };
+
+  $scope.subPage = function(){
+    console.log('works', $scope.currentPage);
+    if($scope.currentPage > 0){
+      $scope.currentPage -= 1;
+      setFilter();
+      setPhoto();
+    }
+};
+
+  $scope.photos = $scope.filterPhotos.map(function(obj){
     return {
       url: obj.images.low_resolution.url,
       username: obj.user.username,
