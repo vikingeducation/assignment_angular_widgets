bf5f6a5ba96eb51ce01a3e0496c80458f8b9de4d
Julia Herron <julia.herron@gmail.com>
2016-03-24 17:06:23 -0700

    filter by tag is half-working

--- app.js
+++ app.js
@@ -50,12 +50,12 @@ widgets.controller( 'PhotosCtrl', [
 
       $scope.photosArr = [];
       $scope.filtersObj= { "All": ""};
-      $scope.tagsObj= { "All": ""};
+      $scope.tagsObj= { " All": ""};
       $scope.currentFilter = "";
+      $scope.selectedTags = [];
+
+
       $scope.rawFeed.data.forEach( function(elt){
-
-        // console.log(elt["likes"]["count"])
-
         $scope.photosArr.push({
           url: elt["images"]["standard_resolution"]["url"],
           username: elt["user"]["username"],
@@ -74,31 +74,64 @@ widgets.controller( 'PhotosCtrl', [
             $scope.tagsObj[ tag ] = tag;
           });
         }
-
       });
-      console.log($scope.tagsObj);
 
       $scope.selectFilter = function( filter ) {
-
         $scope.currentFilter = filter;
+      };
 
-        // $scope.rawFeed.data.forEach( function(elt){
-        //   if ( elt['filter'] === filter ) {
 
-        //     $scope.photosArr.push({
-        //       url: elt["images"]["standard_resolution"]["url"],
-        //       username: elt["user"]["username"],
-        //       date: (elt["created_time"]*1000),
-        //       likes: elt["likes"]["count"],
-        //       comments: elt["comments"]["count"],
-        //       link: elt["link"],
-        //       hashtags: elt["tags"],
-        //       filter: elt["filter"]
-        //     });
-        //   }
+      $scope.selectTag = function(tag) {
+        console.log($scope.selectedTags)
+      }
 
-        // };
-
-    };
   }
 ]);
+
+
+
+widgets.filter('tagFilter', function() {
+  return function(collection, selectedTags) {
+    var filteredCollection = [];
+
+    console.log("collection: "+ collection)
+    console.log(selectedTags)
+
+    // angular.forEach(collection, function(photo) {
+
+    for (var j = 0; j < collection.length; j++) {
+      console.log(collection[j]["hashtags"])
+      for (var i = 0; i < selectedTags.length; i++ ) {
+
+        if (collection[j]["hashtags"].indexOf(selectedTags[i]) > -1) {
+          filteredCollection.push(collection[j]);
+        }
+      }
+    }
+      console.log(filteredCollection)
+  return filteredCollection;
+
+  }
+})
+
+
+
+//   yourApp.filter('filterPlayful', function() {
+//   // `activatePlayfulFilter` is a boolean the user
+//   //    can set in the DOM to turn on this filter
+//   return function( collection, activatePlayfulFilter ) {
+//     // Only run the filter if it has been activated
+//     //   by the optional parameter we take
+//     if( !activatePlayfulFilter ){ return false }
+
+//     var filteredCollection = []
+
+//     angular.forEach( collection, function( kitten ){
+//       // If the kitten is playful, keep it!
+//       if( kitten.Playfulness == "Playful" ){
+//         filteredCollection.push( kitten ); 
+//       }
+//     })
+//     return filteredCollection;
+//   };
+// });
