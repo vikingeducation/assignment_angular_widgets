beb772d8ebd263d154939c400b933b71efb089a4
Steven Chang <prime_pork@hotmail.com>
2016-09-01 13:30:38 +1000

    user can add images now

--- app.js
+++ app.js
@@ -5,10 +5,30 @@ app.controller("RestaurantCtrl", ['$scope', function($scope){
 	$scope.restaurants = [];
 	$scope.name = "";
 	$scope.type = "";
+	$scope.imageUrl = "";
+
+	$scope.returnNewRestaurantObject = function(){
+		return {
+			name: $scope.name,
+			foodType: $scope.type,
+			imageUrl: $scope.imageUrl,
+		};
+	};
+
 	$scope.createRestaurant = function(){
-		var restaurant = { name: $scope.name, foodType: $scope.type };
+		var restaurant = $scope.returnNewRestaurantObject();
 		$scope.restaurants.push(restaurant);
+		$scope.resetForm();
+	};
+
+	$scope.deleteRestaurant = function( index ){
+		$scope.restaurants.splice(index, 1);
+	};
+
+	$scope.resetForm = function(){
+		$scope.imageUrl = "";
 		$scope.name = "";
 		$scope.type = "";
 	};
+
 }]);
