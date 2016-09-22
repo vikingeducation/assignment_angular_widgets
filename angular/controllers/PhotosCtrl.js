e145f015b1658aea77cd368192dd24d4a5c7ce88
“Matthew <matthew.hinea@gmail.com>
2016-09-22 15:48:57 -0700

    debugging multitag filter

--- angular/controllers/PhotosCtrl.js
+++ angular/controllers/PhotosCtrl.js
@@ -16,22 +16,56 @@ app.controller("PhotosCtrl", ["$scope", "instagramResponse", function($scope, in
     }
     return filterList;
   })();
+
+  $scope.comparePhotoAndSelectedTags = function (photoTag, selectedTags) {
+    for(var i= 0; i < selectedTags.length; i++) {
+      var thisSelectedTag = selectedTags[i];
+      if (thisSelectedTag === photoTag) {
+        return true
+      }
+    }
+    return false
+  }
+
 }]);
 
-app.filter('tagFilter', function(){
-  return function( collection, selectedTags ){
+app.filter('filterMultipleTags', function() {
+
+  var comparePhotoAndSelectedTags = function (photoTag, selectedTags) {
+    for(var i= 0; i < selectedTags.length; i++) {
+      var thisSelectedTag = selectedTags[i];
+      if (thisSelectedTag === photoTag) {
+        return true
+      }
+    }
+    return false
+  };
+
+  return function(collection, selectedTags){
+
+    if (!selectedTags) return [];
 
     var filteredCollection = [];
 
     angular.forEach(collection, function(photo){
-      for(var i= 0; i < selectedTags.length; i++){
+      if (photo.tags.length) {
+        filteredCollection.push(photo);
+        console.log(filteredCollection[0]); // why is filtered collection always empty?
+        for (var j = 0; j < photo.tags.length; j++) {
+          var thisPhotoTag = photo.tags[j];
 
-
-
-        photo.tags
+          var represented = comparePhotoAndSelectedTags(thisPhotoTag, selectedTags);
+          console.log(represented);
+        }
+          // console.log(filteredCollection[0]);  
       }
+      if (!represented) {
+        filteredCollection.pop();
+      }    
     }
-
+      )
+    console.log(filteredCollection);
+    return filteredCollection;
   }
-
 })
+
