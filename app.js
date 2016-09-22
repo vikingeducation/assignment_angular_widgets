772b82dd951df6207bcc18060cfdea5b44c0d90c
cjvirtucio87 <cjv287@gmail.com>
2016-09-22 11:02:39 -0700

    Got details.

--- app.js
+++ app.js
@@ -27,11 +27,25 @@ widgets.controller('RestaurantCtrl', ['$scope', '_', function($scope, _) {
 
 widgets.controller('PhotosCtrl', ['$scope', '_', function($scope, _) {
 
+  // Data:
+  // user
+  // time
+  // like
+  // comment count
+  // other stuff
+
+  // Click to see image.
+
   $scope.rawFeed = instagramResponse;
   $scope.pictures = []
   for (var i = 0; i < $scope.rawFeed.data.length; i++) {
     var picture = {};
     picture['url'] = $scope.rawFeed.data[i].images.thumbnail.url;
+    picture['user'] = $scope.rawFeed.data[i].user.username;
+    picture['time'] = $scope.rawFeed.data[i].created_time;
+    picture['likes'] = $scope.rawFeed.data[i].likes.count;
+    picture['comments'] = $scope.rawFeed.data[i].comments.count;
+    picture['link'] = $scope.rawFeed.data[i].link;
     $scope.pictures.push(picture);
   }
 
