ce2408a023ab6dbdc3e82f6bcd14770b75d74d9f
Hannah Squier <hannahsquier@gmail.com>
2016-09-22 09:59:11 -0700

    set up search

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ var widgets = angular.module('widgets', []);
 
 widgets.controller('RestaurantCtrl', ['$scope', function($scope){
   $scope.restaurants = [];
+
+  $scope.searchBy = "Food"
+
   $scope.createRestaurant = function() {
     var restaurant = {
       name: $scope.name,
-      foodType: $scope.food
+      foodType: $scope.food,
+      imgUrl: $scope.imgUrl
     };
     $scope.restaurants.push(restaurant);
     $scope.name = '';
     $scope.food = '';
+    $scope.imgUrl = "";
+  };
+
+  $scope.deleteRestaurant = function(i) {
+    $scope.restaurants.splice(i,1)
+  };
+
+  $scope.sortElements = function(e) {
+
+    $scope.searchBy = angular.element(e.target).text()
   };
 }]);
