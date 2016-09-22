a90649f5ae21a5c06d6aeff3627a54445eab0c4e
jsteenb2 <jonathansteenbergen@gmail.com>
2016-09-22 11:38:45 -0700

    added selects

--- angular/controllers/PhotosCtrl.js
+++ angular/controllers/PhotosCtrl.js
@@ -1,4 +1,12 @@
 app.controller("PhotosCtrl", ["$scope", "instagramResponse", function($scope, instagramResponse) {
   $scope.rawFeed = instagramResponse;
-
-}])
+  $scope.tags = (function(){
+    var tagList = [];
+    for(var i = 0; i < $scope.rawFeed.data.length; i++){
+      for(var j = 0; j < $scope.rawFeed.data[i].tags.length; j++){
+        tagList.push($scope.rawFeed.data[i].tags[j]);
+      }
+    }
+    return tagList;
+  })();
+}]);
