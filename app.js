1d970c0edbe6da8f0e5615d0fd334de10fc6434c
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-09-22 10:20:37 -0700

    Sorts by filter

--- app.js
+++ app.js
@@ -4,16 +4,28 @@ widgets.controller('Restaurant', ["$scope", function($scope) {
   $scope.restaurants = [];
 
   $scope.createRestaurant = function() {
-    var object = {}
+    var object = {};
     object.name = $scope.name;
     object.food = $scope.food;
+    object.image = $scope.image;
     $scope.restaurants.push(object);
     $scope.name = "";
     $scope.food = "";
+    $scope.image = "";
   };
 
   $scope.deleteRestaurant = function(name, food) {
-    for(var i = 0;)
-  }
+    for (var i = 0; i < $scope.restaurants.length; i++) {
+      if ($scope.restaurants[i].name === name &&
+          $scope.restaurants[i].food === food) {
+        $scope.restaurants.splice(i, 1);
+        break;
+      }
+    }
+  };
+
+  $scope.changeFilter = function(filter) {
+    $scope.restaurantFilter = filter;
+  };
 
 }]);
