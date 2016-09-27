f400eb60231b5ea812537c60654d9aee71be3c2c
schoetlr <srlrs20020@gmail.com>
2016-09-27 12:08:56 -0400

    filter pics by clicking on user

--- app.js
+++ app.js
@@ -28,3 +28,45 @@ widgets.controller('RestaurantCtrl',
       $scope.restaurants.splice(index, 1);
     };
   }]);
+
+
+
+
+widgets.controller('PhotosCtrl',
+  ['$scope', function($scope){
+
+    $scope.rawFeed = instagramResponse;
+    $scope.selectedFilter = "";
+    $scope.selectedUser = "";
+    $scope.allImages = [];
+    $scope.filters = [];
+    $scope.hashtags = [];
+    $scope.users = [];
+
+    for(var i = 0; i < $scope.rawFeed.data.length; i++){
+      var data = $scope.rawFeed.data[i];
+      var userData = data.user;
+      var image = {};
+      var user = {};
+
+      image.url = data.images.thumbnail.url;
+      image.user = data.user.username;
+      image.time = data.created_time;
+      image.likes = data.likes.count;
+      image.comments = data.comments.count;
+      image.link = data.link;
+      image.filter = data.filter;
+      image.hashtags = data.tags;
+
+      user.userName = userData.username;
+      user.profilePicture = userData.profile_picture;
+      user.fullName = userData.full_name
+
+      $scope.users.push(user);
+      $scope.hashtags = $scope.hashtags.concat(image.hashtags);
+      $scope.filters.push(image.filter);
+      $scope.allImages.push(image);
+    };
+
+    $scope.images = $scope.allImages;
+  }]);
