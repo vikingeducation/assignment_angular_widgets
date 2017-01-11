2669ff71d1e62490ff8447822ad2a05d3054e7af
Farruh <ufarruh@gmail.com>
2017-01-11 06:24:12 -0800

    photos done

--- photo/app.js
+++ photo/app.js
@@ -2,5 +2,9 @@ angular.module("InstaCopy", [])
   .controller("PhotosCtrl", ["$scope", function($scope){
     $scope.photos = instagramResponse.data;
 
+    $scope.openUrl = function (ur) {
+      $window.open(url, "_blank")
+    }
+
 
   }]);
