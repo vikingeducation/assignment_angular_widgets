c34ebc713273b1e37841b55ccaa7292b025a207b
John Sawyer <sawyer@sawyermerchant.com>
2017-03-25 20:39:36 -0400

    Add filter to sort by name or type

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ var widgets = angular.module('widgets', []);
 
 widgets.controller('restaurantCtrl', ['$scope', function($scope){
 
-  $scope.restaurants = [{
-    name: "test",
-    type: "Testican"
-  }];
+  $scope.restaurants = [
+    {
+      name: "test",
+      type: "Testican"
+    },
+    {
+      name: "test2",
+      type: "Italian"
+    },
+    {
+      name: "test the third",
+      type: "Mexican"
+    },
+    {
+      name: "fourth test",
+      type: "Italian"
+    }
+  ];
+
   $scope.createRestaurant = function(){
     if ($scope.name && $scope.type) {
       var newRestaurant = {
@@ -15,6 +30,17 @@ widgets.controller('restaurantCtrl', ['$scope', function($scope){
       $scope.restaurants.push(newRestaurant);
       $scope.name = "";
       $scope.type = "";
+    }
+  };
+
+  $scope.order = 'type';
+
+  $scope.sort = function(column) {
+    if ($scope.order === column) {
+      $scope.direction = !$scope.direction;
+    } else {
+      $scope.direction = false;
+      $scope.order = column;
     }
   };
 }]);
