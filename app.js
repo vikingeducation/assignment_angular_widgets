638412e8bc7cb09da49d40f50b5d52de1faf8393
Kenneth Kottenstette <kennykotten@gmail.com>
2017-05-02 23:59:02 -0700

    table go back to

--- app.js
+++ app.js
@@ -2,4 +2,25 @@ var widgets = angular.module('WidgetsApp', []);
 
 widgets.controller('RestaurantCtrl', ['$scope', function($scope){
   $scope.restaurants = [];
+
+  $scope.clearInputs = function(){
+    $scope.name = '';
+    $scope.typeOfFood = '';
+    $scope.imageUrl = '';
+  };
+
+  $scope.destroy = function(index){
+    $scope.restaurants.splice(index, 1);
+  };
+
+  $scope.createRestaurant = function(){
+    var restaurant = {};
+    restaurant.name = $scope.name;
+    restaurant.type = $scope.typeOfFood;
+    restaurant.imageUrl = $scope.imageUrl;
+    $scope.restaurants.push(restaurant);
+
+    $scope.clearInputs();
+  };
+
 }]);
